ea38a47e38d1f44ab00a7ce2f5fda7036f5f20f5
manyong-Han <manyong2486@naver.com>
2020-03-04 23:18:20 +0900

    Military Schedule Management
    * Team Toy Project *
    Military Schedule Management

---
+++ app/src/main/java/com/teamproject/aaaaan_2/ui/menu/BlankFragment.java
@@ -0,0 +1,24 @@
+package com.teamproject.aaaaan_2.ui.menu;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.teamproject.aaaaan_2.R;
+
+/**
+ * Created by hanman-yong on 2020-01-15.
+ */
+public class BlankFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+}
